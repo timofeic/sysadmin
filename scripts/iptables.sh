2b36d4d005a49620f72e038de762ba4d8d66b80b
Timothy Cheung <thcheung@gmail.com>
2015-02-22 21:40:31 +0000

    Update iptables.sh
    Removed outgoing rules as outgoing is not blocked.

--- scripts/iptables.sh
+++ scripts/iptables.sh
@@ -6,37 +6,27 @@ iptables -L
 # Flush existing table
 iptables -F
 
-# Drop Foward and Accept Output
+iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
 
-# Allow Incoming, outgoing SSH, port 22
+# Allow Incoming SSH, port 22
 iptables -A INPUT -i eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
-iptables -A OUTPUT -o eth0 -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -i eth0 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
-
-# Allow outbound DNS, udp port 53
-iptables -A OUTPUT -p udp -o eth0 --dport 53 -j ACCEPT
+# Allow inbound DNS, udp port 53
 iptables -A INPUT -p udp -i eth0 --sport 53 -j ACCEPT
 
 # Allow Incoming, Outgoing HTTP, port 80
 iptables -A INPUT -i eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 
-iptables -A OUTPUT -o eth0 -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
 
 # Allow Incoming, Outgoing HTTPs, port 443
 iptables -A INPUT -i eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -o eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
 
-iptables -A OUTPUT -o eth0 -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -i eth0 -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
 
 # Allow ping from inside to outside
-iptables -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
 # Loopback
@@ -45,10 +35,6 @@ iptables -A OUTPUT -o lo -j ACCEPT
 
 # Simple DoS prevention - limit 30 connections/min after 100 connections have been made
 iptables -A INPUT -p tcp --dport 80 -m limit --limit 30/minute --limit-burst 100 -j ACCEPT
-
-# Now set default policy to drop all other Input.
-# This is deliberately not executed above so that the script can be excuted remotely.
-iptables -P INPUT DROP
 
 # Optional - depending on the server
 # Allow ping from outside to inside
